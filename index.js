d680c22e44fd129e727443cc93c2eb3486e4f25f
Shofiya2003 <shofiyabootwala@gmail.com>
2023-04-19 19:55:30 +0530

    connected to mongoose database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose')
 
 require('dotenv').config();
 
@@ -13,6 +14,16 @@ app.use(bodyparser.json())
 app.use(cors());
 
 app.use('/', api)
+
+//connecting to database
+mongoose.connect('mongodb://localhost:27017/spinderDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("connected to database")
+}).catch(err => {
+    console.log(`error connecting to database ${err}`);
+})
 
 const port = process.env.PORT || 8000
 
