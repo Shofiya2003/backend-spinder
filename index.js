1bbbb6fbd22b968ccf436e1f348e659fb3a1ad71
Shofiya2003 <shofiyabootwala@gmail.com>
2023-04-19 15:23:17 +0530

    imported spotify auth route and used it as middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const api = require('./routes/api')
 
 const app = express();
@@ -10,7 +12,7 @@ app.use(bodyparser.json())
 
 app.use(cors());
 
-app.use('/api/v1',api)
+app.use('/', api)
 
 const port = process.env.PORT || 8000
 
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
 const router = express.Router();
-
-
+const auth = require('../routes/spotify_auth')
+router.use('/spotify', auth)
 router.get('/', (req, res) => {
     res.send("we are logically blessed");
 });
