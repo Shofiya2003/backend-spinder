6b7a01809d793a373072ff3b41560740336b0091
Shofiya2003 <shofiyabootwala@gmail.com>
2023-04-19 13:44:01 +0530

    added the get root route

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 
+const api = require('./routes/api')
+
 const app = express();
 
 app.use(bodyparser.json())
 
 app.use(cors());
+
+app.use('/api/v1',api)
 
 const port = process.env.PORT || 8000
 
