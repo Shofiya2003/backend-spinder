e828b06e09055a7a08b36d95e76c09993400a10b
Shofiya2003 <shofiyabootwala@gmail.com>
2023-04-19 19:54:32 +0530

    added schema for user model

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema({
+    email: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    spotifyId: {
+        type: String,
+        required: true
+    },
+    artistScore: {
+        type: Map,
+        required: true
+    },
+
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('user', user);
