499544a422cb7a91f8d8b45ec5fc2950094e7506
Shofiya2003 <shofiyabootwala@gmail.com>
2023-04-25 21:33:54 +0530

    refactor:changed the api route

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require('../routes/spotify_auth')
 const users = require('../routes/user');
 router.use('/spotify', auth)
-router.use('/users', users)
+router.use('/user', users)
 router.get('/', (req, res) => {
     res.send("we are logically blessed");
 });
